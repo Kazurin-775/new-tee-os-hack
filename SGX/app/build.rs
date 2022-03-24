5257a77a409b01c641904b392820b3ac3da1117a
Kazurin Nanako <71819243+Kazurin-775@users.noreply.github.com>
2022-03-24 19:55:36 +0800

    Cleanup SGX part & make it work

--- SGX/app/build.rs
+++ SGX/app/build.rs
@@ -1,21 +1,18 @@
 use std::env;
 
-
-fn main () {
-
-    let sdk_dir = env::var("SGX_SDK")
-                    .unwrap_or_else(|e| e.to_string());
-    let is_sim = env::var("SGX_MODE")
-                    .unwrap_or_else(|e| e.to_string());
+fn main() {
+    let sdk_dir = env::var("SGX_SDK").unwrap();
+    let sgx_mode = env::var("SGX_MODE").unwrap();
     println!("cargo:rustc-link-search=native=../lib");
-    println!("cargo:rustc-link-search=native={}/lib64", "/home/lx/Downloads/SGX/sgxsdk/");
+    println!("cargo:rustc-link-search=native={}/lib64", sdk_dir);
     println!("cargo:rustc-link-search=native=../lib/mesalock-rt/");
 
     println!("cargo:rustc-link-lib=static=Enclave_u");
     println!("cargo:rustc-link-lib=static=test");
 
-    match is_sim.as_ref() {
-        "HW" => println!("cargo:rustc-link-lib=dylib=sgx_urts"),
-        _    => println!("cargo:rustc-link-lib=dylib=sgx_urts_sim"),
+    let sgx_libs = ["sgx_urts", "sgx_launch"];
+    let lib_suffix = (if sgx_mode == "HW" { "" } else { "_sim" });
+    for lib in sgx_libs {
+        println!("cargo:rustc-link-lib=dylib={}{}", lib, lib_suffix);
     }
 }
