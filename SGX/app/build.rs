ae630492f1c81554c60dc8fbf80ea1e1cece08e7
Kazurin Nanako <71819243+Kazurin-775@users.noreply.github.com>
2022-03-30 22:49:12 +0800

    sgx: Refactor and cleanup `app`

--- SGX/app/build.rs
+++ SGX/app/build.rs
@@ -1,8 +1,8 @@
 use std::env;
 
 fn main() {
-    let sdk_dir = env::var("SGX_SDK").unwrap();
-    let sgx_mode = env::var("SGX_MODE").unwrap();
+    let sdk_dir = env::var("SGX_SDK").unwrap_or_else(|_| "/opt/intel/sgxsdk".to_string());
+    let sgx_mode = env::var("SGX_MODE").unwrap_or_else(|_| "SIM".to_string());
     println!("cargo:rustc-link-search=native=../lib");
     println!("cargo:rustc-link-search=native={}/lib64", sdk_dir);
     println!("cargo:rustc-link-search=native=../lib/mesalock-rt/");
@@ -11,7 +11,7 @@ fn main() {
     println!("cargo:rustc-link-lib=static=test");
 
     let sgx_libs = ["sgx_urts", "sgx_launch"];
-    let lib_suffix = (if sgx_mode == "HW" { "" } else { "_sim" });
+    let lib_suffix = if sgx_mode == "HW" { "" } else { "_sim" };
     for lib in sgx_libs {
         println!("cargo:rustc-link-lib=dylib={}{}", lib, lib_suffix);
     }
--- SGX/app/src/main.rs
+++ SGX/app/src/main.rs
@@ -1,17 +1,16 @@
-extern crate sgx_types;
-extern crate sgx_urts;
-extern crate libc;
-
 use sgx_types::*;
 use sgx_urts::SgxEnclave;
-use std::fs::File;
-use std::io::Read;
-static ENCLAVE_FILE: &'static str = "enclave.signed.so";
-const SHARED_MEM_SIZE:usize=0x400;
 
-extern {
-    fn rt_main(eid: sgx_enclave_id_t, retval: *mut sgx_status_t,
-        sharemem: *mut u8, memsz: usize) -> sgx_status_t;
+const ENCLAVE_FILE: &str = "enclave.signed.so";
+const SHARED_MEM_SIZE: usize = 0x400;
+
+extern "C" {
+    fn rt_main(
+        eid: sgx_enclave_id_t,
+        retval: *mut sgx_status_t,
+        sharemem: *mut u8,
+        memsz: usize,
+    ) -> sgx_status_t;
 }
 
 fn init_enclave() -> SgxResult<SgxEnclave> {
@@ -20,53 +19,58 @@ fn init_enclave() -> SgxResult<SgxEnclave> {
     // call sgx_create_enclave to initialize an enclave instance
     // Debug Support: set 2nd parameter to 1
     let debug = 1;
-    let mut misc_attr = sgx_misc_attribute_t {secs_attr: sgx_attributes_t { flags:0, xfrm:0}, misc_select:0};
-    SgxEnclave::create(ENCLAVE_FILE,
-                       debug,
-                       &mut launch_token,
-                       &mut launch_token_updated,
-                       &mut misc_attr)
+    let mut misc_attr = sgx_misc_attribute_t {
+        secs_attr: sgx_attributes_t { flags: 0, xfrm: 0 },
+        misc_select: 0,
+    };
+    SgxEnclave::create(
+        ENCLAVE_FILE,
+        debug,
+        &mut launch_token,
+        &mut launch_token_updated,
+        &mut misc_attr,
+    )
 }
 
-fn main() {
-    let enclave = match init_enclave() {
-        Ok(r) => {
-            println!("[+] Init Enclave Successful {}!", r.geteid());
-            r
-        },
-        Err(x) => {
-            println!("[-] Init Enclave Failed {}!", x.as_str());
-            return;
-        },
+fn main() -> anyhow::Result<()> {
+    env_logger::builder()
+        .filter_level(log::LevelFilter::Trace)
+        .init();
+
+    let enclave = init_enclave()
+        .map_err(|status| anyhow::anyhow!("failed to initialize enclave: {}", status.as_str()))?;
+    log::debug!("Initialized enclave with ID = {}", enclave.geteid());
+
+    let edge_mem = unsafe { libc::malloc(SHARED_MEM_SIZE) };
+    log::debug!("Shared memory allocated, address = {:?}", edge_mem);
+
+    // let mut kernel_file = File::open("sgx-rt.bin").expect("failed to open the bin");
+    // let edge_mem_ref:&mut [u8]=core::slice::from_raw_parts_mut(edge_mem as *mut u8,SHARED_MEM_SIZE);
+    // let bytes_read = kernel_file
+    //     .read(edge_mem_ref)
+    //     .expect("failed to read the bin");
+    // if bytes_read == 0 {
+    //     panic!("failed to read the bin");
+    // }
+
+    let mut retval = sgx_status_t::SGX_ERROR_UNEXPECTED;
+    let sgx_result = unsafe {
+        rt_main(
+            enclave.geteid(),
+            &mut retval,
+            edge_mem as _,
+            SHARED_MEM_SIZE,
+        )
     };
-
-    let result = unsafe {
-        let edge_mem = libc::malloc(SHARED_MEM_SIZE);
-
-        // let mut kernel_file = File::open("sgx-rt.bin").expect("failed to open the bin");
-        // let edge_mem_ref:&mut [u8]=core::slice::from_raw_parts_mut(edge_mem as *mut u8,SHARED_MEM_SIZE);
-        // let bytes_read = kernel_file
-        //     .read(edge_mem_ref)
-        //     .expect("failed to read the bin");
-        // if bytes_read == 0 {
-        //     panic!("failed to read the bin");
-        // }
-        
-        println!("[+] Shared memory allocated! Addr: 0X{:x}",edge_mem as usize);
-
-        let mut retval = sgx_status_t::SGX_SUCCESS;
-        rt_main(enclave.geteid(),
-                    &mut retval,
-                    edge_mem as _, 
-                    SHARED_MEM_SIZE)
-    };
-    match result {
+    match sgx_result {
         sgx_status_t::SGX_SUCCESS => {
-            println!("[+] SGX TEE OS operating successfully...");
-        },
+            log::debug!("SGX TEE OS exited successfully");
+        }
         _ => {
-            println!("[-] ECALL Enclave Failed {}!", result.as_str());
+            log::error!("SGX TEE OS failed with error: {}", sgx_result.as_str());
         }
     }
     enclave.destroy();
+
+    Ok(())
 }
