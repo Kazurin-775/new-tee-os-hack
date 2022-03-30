a5fe09f2610db2a3ab0f3c97252a9febbf383713
Kazurin Nanako <71819243+Kazurin-775@users.noreply.github.com>
2022-03-30 22:54:46 +0800

    sgx: Extract `sgx-runner` crate

---
+++ sgx-runner/src/lib.rs
@@ -0,0 +1,76 @@
+use sgx_types::*;
+use sgx_urts::SgxEnclave;
+
+const ENCLAVE_FILE: &str = "enclave.signed.so";
+const SHARED_MEM_SIZE: usize = 0x400;
+
+extern "C" {
+    fn rt_main(
+        eid: sgx_enclave_id_t,
+        retval: *mut sgx_status_t,
+        sharemem: *mut u8,
+        memsz: usize,
+    ) -> sgx_status_t;
+}
+
+fn init_enclave() -> SgxResult<SgxEnclave> {
+    let mut launch_token: sgx_launch_token_t = [0; 1024];
+    let mut launch_token_updated: i32 = 0;
+    // call sgx_create_enclave to initialize an enclave instance
+    // Debug Support: set 2nd parameter to 1
+    let debug = 1;
+    let mut misc_attr = sgx_misc_attribute_t {
+        secs_attr: sgx_attributes_t { flags: 0, xfrm: 0 },
+        misc_select: 0,
+    };
+    SgxEnclave::create(
+        ENCLAVE_FILE,
+        debug,
+        &mut launch_token,
+        &mut launch_token_updated,
+        &mut misc_attr,
+    )
+}
+
+pub fn main() -> anyhow::Result<()> {
+    env_logger::builder()
+        .filter_level(log::LevelFilter::Trace)
+        .init();
+
+    let enclave = init_enclave()
+        .map_err(|status| anyhow::anyhow!("failed to initialize enclave: {}", status.as_str()))?;
+    log::debug!("Initialized enclave with ID = {}", enclave.geteid());
+
+    let edge_mem = unsafe { libc::malloc(SHARED_MEM_SIZE) };
+    log::debug!("Shared memory allocated, address = {:?}", edge_mem);
+
+    // let mut kernel_file = File::open("sgx-rt.bin").expect("failed to open the bin");
+    // let edge_mem_ref:&mut [u8]=core::slice::from_raw_parts_mut(edge_mem as *mut u8,SHARED_MEM_SIZE);
+    // let bytes_read = kernel_file
+    //     .read(edge_mem_ref)
+    //     .expect("failed to read the bin");
+    // if bytes_read == 0 {
+    //     panic!("failed to read the bin");
+    // }
+
+    let mut retval = sgx_status_t::SGX_ERROR_UNEXPECTED;
+    let sgx_result = unsafe {
+        rt_main(
+            enclave.geteid(),
+            &mut retval,
+            edge_mem as _,
+            SHARED_MEM_SIZE,
+        )
+    };
+    match sgx_result {
+        sgx_status_t::SGX_SUCCESS => {
+            log::debug!("SGX TEE OS exited successfully");
+        }
+        _ => {
+            log::error!("SGX TEE OS failed with error: {}", sgx_result.as_str());
+        }
+    }
+    enclave.destroy();
+
+    Ok(())
+}
