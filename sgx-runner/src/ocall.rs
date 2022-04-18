96085be637011a1b6297b3b8b245e38261617913
Kazurin Nanako <71819243+Kazurin-775@users.noreply.github.com>
2022-04-18 20:00:21 +0800

    sgx: Remove obsolete `ocall_syscall_write`

--- sgx-runner/src/ocall.rs
+++ sgx-runner/src/ocall.rs
@@ -1,9 +1,4 @@
 #[no_mangle]
-unsafe extern "C" fn ocall_syscall_write(fd: i32, buf: *const i8, count: usize) -> i64 {
-    libc::write(fd, buf.cast(), count) as i64
-}
-
-#[no_mangle]
 unsafe extern "C" fn ocall_edge_kick() {
     if let Err(err) = edge_responder::handle_edge_call(&mut *crate::EDGE_MEM.lock().unwrap()) {
         log::error!("Failed to handle edge call: {:#}", err);
